be46cdc9a932f0e480ba4ee5db551974bbeccb5f
Ishani Phukan <Ishani.Phukan28@gmail.com>
2024-02-03 12:36:45 +0530

    Added the C program

---
+++ MPT.c
@@ -0,0 +1,559 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    // Modern Periodic Table : User can search an element properties by its atomic number
+    
+    int no;
+    printf("Enter the atomic number of the element to be searched for:");
+     scanf("%d",&no);
+    switch(no){
+        case 1: printf("Name: Hydrogen\n");
+             printf("Symbol:H\n");
+             printf("Metallic Property: Reactive Non-Metal\n");
+             break;
+        case 2: printf("Name: Helium\n");
+             printf("Symbol: He\n");
+             printf("Metallic Property: Noble Gases\n");
+             break;
+        case 3:printf("Name:Lithium \n");
+             printf("Symbol:Li\n");
+             printf("Metallic Property: Alkali Metal\n");
+             break;
+        case 4:printf("Name: Beryllium\n");
+             printf("Symbol:Be\n");
+             printf("Metallic Property: Alkaline Earth Metal\n");
+             break;
+        case 5:printf("Name: Boron\n");
+             printf("Symbol: B\n");
+             printf("Metallic Property: Metalloid\n");
+             break;
+        case 6:printf("Name: Carbon\n");
+             printf("Symbol:C\n");
+             printf("Metallic Property: Reactive Non-Metal\n");
+             break;
+        case 7:printf("Name: Nitrogen\n");
+             printf("Symbol:N\n");
+             printf("Metallic Property:Reactive Non-Metal\n");
+             break;
+        case 8 :printf("Name: Oxygen\n");
+             printf("Symbol:O\n");
+             printf("Metallic Property:Reactive Non-Metal\n");
+             break;
+        case 9 :printf("Name:Fluorine\n");
+             printf("Symbol:F\n");
+             printf("Metallic Property:Reactive Non-Metal\n");
+             break;
+        case 10 :printf("Name:Neon\n ");
+             printf("Symbol:Ne\n");
+             printf("Metallic Property:Nobel Gases\n");
+             break;
+   
+        case 11 :printf("Name:Sodium\n ");
+             printf("Symbol:Na\n");
+             printf("Metallic Property:Alkali Metal\n");
+             break;
+   
+        case 12 :printf("Name:Magnesium\n ");
+             printf("Symbol:Mg\n");
+             printf("Metallic Property:Alkaline Earth Metals\n");
+             break;
+   
+        case 13:printf("Name:Aluminium\n ");
+             printf("Symbol:Al\n");
+             printf("Metallic Property:Post Transition Metals\n");
+             break;
+   
+        case 14 :printf("Name: Silicon\n ");
+             printf("Symbol:Si\n");
+             printf("Metallic Property:Metalloids\n");
+             break;
+   
+        case 15 :printf("Name:Phosphorous\n ");
+             printf("Symbol:P\n");
+             printf("Metallic Property: Reactive Non-Metal\n");
+             break;
+   
+        case 16 :printf("Name:Sulphur\n ");
+             printf("Symbol:S\n");
+             printf("Metallic Property:Reactive Non-Metal\n");
+             break;
+   
+        case 17 :printf("Name:Chlorine\n ");
+             printf("Symbol:Cl\n");
+             printf("Metallic Property:Reactive Non-Metal\n");
+             break;
+   
+        case 18 :printf("Name:Argon\n ");
+             printf("Symbol:Ar\n");
+             printf("Metallic Property:Nobel Gas\n");
+             break;
+   
+        case 19 :printf("Name:Potassium\n ");
+             printf("Symbol:K\n");
+             printf("Metallic Property:Alkali Metals\n");
+             break;
+   
+        case 20 :printf("Name:Calcium\n ");
+             printf("Symbol:Ca\n");
+             printf("Metallic Property:Alkaline Earth Metal\n");
+             break;
+
+        case 21 :printf("Name:Scandium\n ");
+             printf("Symbol:Sc\n");
+             printf("Metallic Property:Transition Metal\n");
+             break;
+
+        case 22 :printf("Name:Titanium\n ");
+             printf("Symbol:Ti\n");
+             printf("Metallic Property:Transition Metal\n");
+             break;
+
+        case 23 :printf("Name:Vanadium\n ");
+             printf("Symbol:V\n");
+             printf("Metallic Property:Transition Metal\n");
+             break;
+
+        case 24 :printf("Name:Chromium\n ");
+             printf("Symbol:Cr\n");
+             printf("Metallic Property:Transition Metal\n");
+             break;
+
+        case 25 :printf("Name:Manganese\n ");
+             printf("Symbol:Mn\n");
+             printf("Metallic Property:Transition Metal\n");
+             break;
+
+        case 26 :printf("Name:Iron\n ");
+             printf("Symbol:Fe\n");
+             printf("Metallic Property:Transition Metal\n");
+             break;
+
+        case 27 :printf("Name:Cobalt\n ");
+             printf("Symbol:Co\n");
+             printf("Metallic Property:Transition Metal\n");
+             break;
+
+        case 28 :printf("Name:Nickel\n ");
+             printf("Symbol:Ni\n");
+             printf("Metallic Property:Transition Metal\n");
+             break;
+
+        case 29 :printf("Name:Copper\n ");
+             printf("Symbol:Cu\n");
+             printf("Metallic Property:Transition Metal\n");
+             break;
+   
+        case 30 :printf("Name:Zinc\n ");
+             printf("Symbol:Zn\n");
+             printf("Metallic Property:Transition Metal\n");
+             break;
+
+        case 31 :printf("Name:Gallium\n ");
+             printf("Symbol:Ga\n");
+             printf("Metallic Property:Post Transition Metal\n");
+             break;
+
+        case 32 :printf("Name:Germanium\n ");
+             printf("Symbol:Ge\n");
+             printf("Metallic Property:Metalloid\n");
+             break;
+
+        case 33:printf("Name:Arsenic\n ");
+             printf("Symbol:As\n");
+             printf("Metallic Property:Metalloid\n");
+             break;
+
+        case 34 :printf("Name:Selenium\n ");
+             printf("Symbol:Se\n");
+             printf("Metallic Property:Reactive Non-Metal\n");
+             break;
+
+        case 35 :printf("Name:Bromine\n ");
+             printf("Symbol:Br\n");
+             printf("Metallic Property:Reactive Non-Metal\n");
+             break;
+
+        case 36 :printf("Name:Krypton\n ");
+             printf("Symbol:Kr\n");
+             printf("Metallic Property:Nobel Gas\n");
+             break;
+
+        case 37 :printf("Name:Rubidium\n ");
+             printf("Symbol:Rb\n");
+             printf("Metallic Property:Alkali Metals\n");
+             break;
+   
+        
+        case 38 :printf("Name:Strontium\n ");
+             printf("Symbol:Sr\n");
+             printf("Metallic Property:Alkaline Earth Metals\n");
+             break;
+
+        case 39 :printf("Name:Yttrium\n ");
+             printf("Symbol:Y\n");
+             printf("Metallic Property:Transition Metals\n");
+             break;
+
+        case 40:printf("Name:Zirconium\n ");
+             printf("Symbol:Zr\n");
+             printf("Metallic Property:Transition Metal\n");
+             break;
+
+        case 41 :printf("Name:Niobium\n ");
+             printf("Symbol:Nb\n");
+             printf("Metallic Property:Transition Metal\n");
+             break;
+
+        case 42:printf("Name:Molybdenum\n ");
+             printf("Symbol:Mo\n");
+             printf("Metallic Property:Transition Metal\n");
+             break;
+
+        case 43:printf("Name:Technetium\n ");
+             printf("Symbol:Tc\n");
+             printf("Metallic Property:Transition Metal\n");
+             break;
+
+        case 44:printf("Name:Ruthenium\n ");
+             printf("Symbol:Ru\n");
+             printf("Metallic Property:Transition Metal\n");
+             break;
+
+        case 45:printf("Name: Rhodium\n ");
+             printf("Symbol: Rh\n");
+             printf("Metallic Property: Transition Metal\n");
+             break;
+
+        case 46:printf("Name: Palladium\n ");
+             printf("Symbol: Pd\n");
+             printf("Metallic Property: Transition Metal\n");
+             break;
+
+        case 47:printf("Name:Silver\n ");
+             printf("Symbol:Ag\n");
+             printf("Metallic Property:Transition Metal\n");
+             break;
+
+        case 48:printf("Name:Cadmium\n ");
+             printf("Symbol:Cd\n");
+             printf("Metallic Property:Transition Metal\n");
+             break;
+
+        case 49:printf("Name:Indium\n ");
+             printf("Symbol:In\n");
+             printf("Metallic Property:Post Transition Metal\n");
+             break;
+
+        case 50:printf("Name:Tin\n ");
+             printf("Symbol:Sn\n");
+             printf("Metallic Property:Post Transition Metal\n");
+             break;
+
+        case 51:printf("Name:Antimony\n ");
+             printf("Symbol:Sb\n");
+             printf("Metallic Property:Metalloid\n");
+             break;
+
+        case 52:printf("Name:Tellurium\n ");
+             printf("Symbol:Te\n");
+             printf("Metallic Property:Metalloid\n");
+             break;
+
+        case 53:printf("Name:Iodine\n ");
+             printf("Symbol:I\n");
+             printf("Metallic Property:Reactive Non-Metal\n");
+             break;
+
+        case 54:printf("Name:Xenon\n ");
+             printf("Symbol:Xe\n");
+             printf("Metallic Property:Noble Gas\n");
+             break;
+
+        case 55:printf("Name:Caesium\n ");
+             printf("Symbol:Cs\n");
+             printf("Metallic Property:Alkali Metals\n");
+             break;
+
+        case 56:printf("Name:Barium\n ");
+             printf("Symbol:Ba\n");
+             printf("Metallic Property:Alkaline Earth Metal\n");
+             break;
+
+        case 57:printf("Name:\n ");
+             printf("Symbol:\n");
+             printf("Metallic Property:\n");
+             break;
+
+        case 58:printf("Name:\n ");
+             printf("Symbol:\n");
+             printf("Metallic Property:\n");
+             break;
+
+        case 59:printf("Name:\n ");
+             printf("Symbol:\n");
+             printf("Metallic Property:\n");
+             break;
+   
+        case 60:printf("Name:\n ");
+             printf("Symbol:\n");
+             printf("Metallic Property:\n");
+             break;
+
+        case 61:printf("Name:\n ");
+             printf("Symbol:\n");
+             printf("Metallic Property:\n");
+             break;
+
+        case 62:printf("Name:\n ");
+             printf("Symbol:\n");
+             printf("Metallic Property:\n");
+             break;
+
+        case 63:printf("Name:\n ");
+             printf("Symbol:\n");
+             printf("Metallic Property:\n");
+             break;
+
+        case 64:printf("Name:\n ");
+             printf("Symbol:\n");
+             printf("Metallic Property:\n");
+             break;
+
+        case 65:printf("Name:\n ");
+             printf("Symbol:\n");
+             printf("Metallic Property:\n");
+             break;
+
+        case 66:printf("Name:\n ");
+             printf("Symbol:\n");
+             printf("Metallic Property:\n");
+             break;
+
+        case 67:printf("Name:\n ");
+             printf("Symbol:\n");
+             printf("Metallic Property:\n");
+             break;
+
+        case 68:printf("Name:\n ");
+             printf("Symbol:\n");
+             printf("Metallic Property:\n");
+             break;
+
+        case 69:printf("Name:\n ");
+             printf("Symbol:\n");
+             printf("Metallic Property:\n");
+             break;
+   
+        case 70:printf("Name:\n ");
+             printf("Symbol:\n");
+             printf("Metallic Property:\n");
+             break;
+             
+        case 71:printf("Name:\n ");
+             printf("Symbol:\n");
+             printf("Metallic Property:\n");
+             break;
+
+        case 72:printf("Name:\n ");
+             printf("Symbol:\n");
+             printf("Metallic Property:\n");
+             break;
+
+        case 73:printf("Name:\n ");
+             printf("Symbol:\n");
+             printf("Metallic Property:\n");
+             break;
+
+        case 74:printf("Name:\n ");
+             printf("Symbol:\n");
+             printf("Metallic Property:\n");
+             break;
+
+        case 75:printf("Name:\n ");
+             printf("Symbol:\n");
+             printf("Metallic Property:\n");
+             break;
+
+        case 76:printf("Name:\n ");
+             printf("Symbol:\n");
+             printf("Metallic Property:\n");
+             break;
+
+        case 77:printf("Name:\n ");
+             printf("Symbol:\n");
+             printf("Metallic Property:\n");
+             break;
+
+        case 78:printf("Name:\n ");
+             printf("Symbol:\n");
+             printf("Metallic Property:\n");
+             break;
+
+        case 79:printf("Name:\n ");
+             printf("Symbol:\n");
+             printf("Metallic Property:\n");
+             break;
+             
+        case 80:printf("Name:\n ");
+             printf("Symbol:\n");
+             printf("Metallic Property:\n");
+             break;
+
+        case 81:printf("Name:\n ");
+             printf("Symbol:\n");
+             printf("Metallic Property:\n");
+             break;
+        case 82:printf("Name:\n ");
+             printf("Symbol:\n");
+             printf("Metallic Property:\n");
+             break;
+        case 83:printf("Name:\n ");
+             printf("Symbol:\n");
+             printf("Metallic Property:\n");
+             break;
+        case 84:printf("Name:\n ");
+             printf("Symbol:\n");
+             printf("Metallic Property:\n");
+             break;
+        case 85:printf("Name:\n ");
+             printf("Symbol:\n");
+             printf("Metallic Property:\n");
+             break;
+        case 86:printf("Name:\n ");
+             printf("Symbol:\n");
+             printf("Metallic Property:\n");
+             break;
+        case 87:printf("Name:\n ");
+             printf("Symbol:\n");
+             printf("Metallic Property:\n");
+             break;
+        case 88:printf("Name:\n ");
+             printf("Symbol:\n");
+             printf("Metallic Property:\n");
+             break;
+        case 89:printf("Name:\n ");
+             printf("Symbol:\n");
+             printf("Metallic Property:\n");
+             break;
+        case 90:printf("Name:\n ");
+             printf("Symbol:\n");
+             printf("Metallic Property:\n");
+             break;
+        case 91:printf("Name:\n ");
+             printf("Symbol:\n");
+             printf("Metallic Property:\n");
+             break;
+        case 92:printf("Name:\n ");
+             printf("Symbol:\n");
+             printf("Metallic Property:\n");
+             break;
+        case 93:printf("Name:\n ");
+             printf("Symbol:\n");
+             printf("Metallic Property:\n");
+             break;
+        case 94:printf("Name:\n ");
+             printf("Symbol:\n");
+             printf("Metallic Property:\n");
+             break;
+        case 95:printf("Name:\n ");
+             printf("Symbol:\n");
+             printf("Metallic Property:\n");
+             break;
+        case 96:printf("Name:\n ");
+             printf("Symbol:\n");
+             printf("Metallic Property:\n");
+             break;
+        case 97:printf("Name:\n ");
+             printf("Symbol:\n");
+             printf("Metallic Property:\n");
+             break;
+        case 98:printf("Name:\n ");
+             printf("Symbol:\n");
+             printf("Metallic Property:\n");
+             break;
+             
+        case 99:printf("Name:\n ");
+             printf("Symbol:\n");
+             printf("Metallic Property:\n");
+             break;
+        case 100:printf("Name:\n ");
+             printf("Symbol:\n");
+             printf("Metallic Property:\n");
+             break;
+        case 101:printf("Name:\n ");
+             printf("Symbol:\n");
+             printf("Metallic Property:\n");
+             break;
+        case 102:printf("Name:\n ");
+             printf("Symbol:\n");
+             printf("Metallic Property:\n");
+             break;
+        case 103:printf("Name:\n ");
+             printf("Symbol:\n");
+             printf("Metallic Property:\n");
+             break;
+        case 104:printf("Name:\n ");
+             printf("Symbol:\n");
+             printf("Metallic Property:\n");
+             break;
+        case 105:printf("Name:\n ");
+             printf("Symbol:\n");
+             printf("Metallic Property:\n");
+             break;
+        case 106:printf("Name:\n ");
+             printf("Symbol:\n");
+             printf("Metallic Property:\n");
+             break;
+        case 107:printf("Name:\n ");
+             printf("Symbol:\n");
+             printf("Metallic Property:\n");
+             break;
+        case 108:printf("Name:\n ");
+             printf("Symbol:\n");
+             printf("Metallic Property:\n");
+             break;
+        case 109:printf("Name:\n ");
+             printf("Symbol:\n");
+             printf("Metallic Property:\n");
+             break;
+        case 110:printf("Name:\n ");
+             printf("Symbol:\n");
+             printf("Metallic Property:\n");
+             break;
+        case 111:printf("Name:\n ");
+             printf("Symbol:\n");
+             printf("Metallic Property:\n");
+             break;
+        case 112:printf("Name:\n ");
+             printf("Symbol:\n");
+             printf("Metallic Property:\n");
+             break;
+        case 113:printf("Name:\n ");
+             printf("Symbol:\n");
+             printf("Metallic Property:\n");
+             break;
+        case 114:printf("Name:\n ");
+             printf("Symbol:\n");
+             printf("Metallic Property:\n");
+             break;
+        case 115:printf("Name:\n ");
+             printf("Symbol:\n");
+             printf("Metallic Property:\n");
+             break;
+        case 116:printf("Name:\n ");
+             printf("Symbol:\n");
+             printf("Metallic Property:\n");
+             break;
+        case 117:printf("Name:\n ");
+             printf("Symbol:\n");
+             printf("Metallic Property:\n");
+             break;
+        case 118:printf("Name:\n ");
+             printf("Symbol:\n");
+             printf("Metallic Property:\n");
+             break;
+
+        default: printf("Element not found! Enter the correct no");
+    }
+    return 0;
+}
